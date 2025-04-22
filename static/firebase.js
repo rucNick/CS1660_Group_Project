c315abeee453198d4f76979764ec5a20db2ccfb5
ML0002 <125226496+ML0002@users.noreply.github.com>
2025-04-22 03:56:22 -0400

    Revert "update"
    This reverts commit 37936bbcb8840b40b53b061c767aecd20a3d61f1.

--- static/firebase.js
+++ static/firebase.js
@@ -13,9 +13,6 @@ function initApp() {
     const signInButton = document.getElementById('signInButton');
     const checkInButton = document.getElementById('checkIn');
     const viewAttendanceButton = document.getElementById('viewAttendance');
-    const attedanceRecordsHeader = document.getElementById('attedanceRecordsHeader');
-    const AttendanceHeader = document.getElementById('AttendanceHeader');
-    const AttendanceSystemHeader = document.getElementById('AttendanceSystemHeader');
     if (user) {
       signInButton.innerText = 'Sign Out';
       document.getElementById('bodyInfo').style.display = '';
@@ -27,18 +24,12 @@ function initApp() {
       if (role === 'Professor') {
         checkInButton.style.display = 'none';
         viewAttendanceButton.style.display = 'block';
-        signInButton.style.display = 'block';
-        AttendanceSystemHeader.style.display = 'block';
         viewAttendanceButton.addEventListener("click", function () {
           viewAttendance(courseId);
         });
       } else if (role === 'Student') {
         viewAttendanceButton.style.display = 'none';
         checkInButton.style.display = 'block';
-        signInButton.style.display = 'block';
-        AttendanceHeader.style.display = 'block';
-        AttendanceSystemHeader.style.display = 'block';
-        attedanceRecordsHeader.style.display = 'none';
       }
     } else {
       signInButton.innerText = 'Sign In with Google';
